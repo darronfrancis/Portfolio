c3ca6b495af3390c2000110b79c087c31c5e8814
darronfrancis <64755534+darronfrancis@users.noreply.github.com>
2020-11-20 19:54:21 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,50 @@
+document.getElementById("bullet1").addEventListener("mouseover", onBullet1);
+document.getElementById("bullet1").addEventListener("mouseout", offBullet1);
+document.getElementById("bullet2").addEventListener("mouseover", onBullet2);
+document.getElementById("bullet2").addEventListener("mouseout", offBullet2);
+document.getElementById("bullet3").addEventListener("mouseover", onBullet3);
+document.getElementById("bullet3").addEventListener("mouseout", offBullet3);
+document.getElementById("bullet4").addEventListener("mouseover", onBullet4);
+document.getElementById("bullet4").addEventListener("mouseout", offBullet4);
+document.getElementById("bullet5").addEventListener("mouseover", onBullet5);
+document.getElementById("bullet5").addEventListener("mouseout", offBullet5);
+document.getElementById("bullet6").addEventListener("mouseover", onBullet6);
+document.getElementById("bullet6").addEventListener("mouseout", offBullet6);
+
+function onBullet1() {
+    document.getElementById("bullet1").style.color = "#66FCF1";
+}
+function offBullet1() {
+    document.getElementById("bullet1").style.color = "#333";
+}
+function onBullet2() {
+    document.getElementById("bullet2").style.color = "#66FCF1";
+}
+function offBullet2() {
+    document.getElementById("bullet2").style.color = "#333";
+}
+function onBullet3() {
+    document.getElementById("bullet3").style.color = "#66FCF1";
+}
+function offBullet3() {
+    document.getElementById("bullet3").style.color = "#333";
+}
+function onBullet4() {
+    document.getElementById("bullet4").style.color = "#66FCF1";
+}
+function offBullet4() {
+    document.getElementById("bullet4").style.color = "#333";
+}
+function onBullet5() {
+    document.getElementById("bullet5").style.color = "#66FCF1";
+}
+function offBullet5() {
+    document.getElementById("bullet5").style.color = "#333";
+}
+function onBullet6() {
+    document.getElementById("bullet6").style.color = "#66FCF1";
+}
+function offBullet6() {
+    document.getElementById("bullet6").style.color = "#333";
+}
+
